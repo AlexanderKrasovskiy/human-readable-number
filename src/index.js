b0dc6e68cde3a30764bdf3ba1a57c618cefe18a3
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-01-07 23:03:37 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(num) {
+  const strNum = num.toString();
   
+  const zeroToTeenArr = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+  ];
+  const decimalsArr = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  if (num < 20) return zeroToTeenArr[num];
+  if (num < 100 && num % 10 === 0) return makeDecimals(strNum[0] - 2);
+  if (num < 100) return `${makeDecimals(strNum[0] - 2)} ${zeroToTeenArr[+strNum[1]]}`;
+  if (num % 100 === 0) return makeHundreds(+strNum[0]);
+  if (num % 10 === 0 && num % 100 >= 20) return `${makeHundreds(+strNum[0])} ${makeDecimals(+strNum[1] - 2)}`;
+  if (num % 100 < 20) return `${makeHundreds(+strNum[0])} ${zeroToTeenArr[+strNum.slice(1)]}`;
+  return `${makeHundreds(+strNum[0])} ${makeDecimals(+strNum[1] - 2)} ${zeroToTeenArr[+strNum[2]]}`;
+
+  function makeDecimals(index) {
+    return decimalsArr[index]
+  }
+  function makeHundreds(index) {
+    return `${zeroToTeenArr[index]} hundred`
+  }
 }
